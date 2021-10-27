60162843d132e4c3f70419427598c634486952c4
Jessica Nguyen <jessica-nguyen@ibm.com>
2021-10-27 15:20:45 -0400

    2nd test red

--- week6/KATA 1/directionreduction.test.js
+++ week6/KATA 1/directionreduction.test.js
@@ -4,4 +4,8 @@ describe('should handle simple cases', () => {
     it ('should return empty array if the input array is empty', () => {
         expect(dirReduc([])).toEqual([]);
     });
+
+    it('should return one direction when input array has one direction', () => {
+        expect(dirReduc(['NORTH'])).toEqual(['NORTH']);
+    });
 });
