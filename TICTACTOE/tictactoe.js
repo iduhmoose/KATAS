1b03ad469c24a9a0d4ad820d14c8045458749c0f
Jessica Nguyen <jessica-nguyen@ibm.com>
2021-11-10 09:07:56 -0500

    1st test - green

--- TICTACTOE/tictactoe.js
+++ TICTACTOE/tictactoe.js
@@ -1,5 +1,7 @@
 function tictactoe() {
-    console.log("A");
+    let gameboard = "Game Board Creation...\n | | \n-+-+-\n | | \n-+-+-\n | | \n\nBoard Created.\nThe game will start with player X";
+    console.log(gameboard);
+    return gameboard;
 }
 
 module.exports = tictactoe;
